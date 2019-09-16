ee122bd72c21fa36738b9c00c3546f536ed8e03e
xzhao4 <xzhao4@OVDIMC10-378V.coast.cccd.adx>
2019-09-16 13:23:41 -0700

    added

--- CS272-H01/src/occ/cs272/h01/Sequence.java
+++ CS272-H01/src/occ/cs272/h01/Sequence.java
@@ -4,8 +4,6 @@
  */
 package occ.cs272.h01;
 
-import java.util.Scanner;
-
 /**
  * @author <xzhao4>
  * @version <09/09/2019>
@@ -18,7 +16,6 @@ public class Sequence
     public static final String ASSIGNMENT = "H01-D";
     
     private int[] array;
-	private static Scanner in;
     
     
     /**
@@ -37,22 +34,22 @@ public class Sequence
     	int[] a = new int[array.length];
     	if(Math.abs(start) <= array.length)
     	{
-    		if(start < 0)
-    		{
-    			for(int i = array.length + start; i < array.length; i++)
-    			{
-    				a[inc] = array[i];
-    				inc++;
-    			}
-    		}
-    		else if (start >= 0)
-    		{
-        			for(int i = start; i < start; i++)
-        			{
-        				a[inc] = array[i];
-        				inc++;
-        			}
-    		}
+    	    if(start >= 0)
+    	    {
+    	        for(int i = start; i < array.length; i++)
+    	        {
+    	            a[inc] = array[i];
+                    inc++;
+    	        }
+    	    }
+    	    else
+    	    {
+    	        for(int i = array.length + start; i < array.length; i++)
+    	        {
+    	            a[inc] = array[i];
+    	            inc++;
+    	        }
+    	    }
     	}
     	return new Sequence(a);
     }
@@ -60,29 +57,39 @@ public class Sequence
     {
     	int inc = 0;
     	int[] a = new int[array.length];
-    	if(start < 0)
+    	if(Math.abs(start) <= array.length)
     	{
-    		for(int i = array.length + start; i < end; i++)
-    		{
-    			a[inc] = array[i];
-    			inc++;
-    		}
-    	}
-    	else if(start >= 0)
-    	{
-    		if(Math.abs(end) >= Math.abs(start))
-    		{
-    			for(int i = start; i < end; i++)
-    			{
-    				a[inc] = array[i];
-    				inc++;
-    			}
-    		}
+    	    for(int i = Math.abs(start); i < end; i++)
+            {
+                a[inc] = array[i];
+                inc++;
+            }
     	}
     	return new Sequence(a);
     }
-    public Sequence slice(int[] a, int[] b, int[] c)
+    public Sequence slice(int start, int end, int step)
     {
+        int inc = 0;
+        int[] a = new int[array.length];
+        if(Math.abs(start) <= array.length)
+        {
+            if(start < 0 || end < 0 || step < 0)
+            {
+                for(int i = array.length + start; i > array.length + end; i += step)
+                {
+                    a[inc] = array[i];
+                    inc++;
+                }
+            }
+            else
+            {
+                for(int i = start; i < end; i += step)
+                {
+                    a[inc] = array[i];
+                    inc++;
+                }
+            }
+        }
     	return new Sequence(a);
     }
     
@@ -93,13 +100,16 @@ public class Sequence
     @Override
     public String toString()
     {
-        String result = "{";
-        if (array.length > 0)
+       String result = "{";
+        if (array.length > 0 && array[0] != 0)
         {
             result += array[0];
             for (int i = 1; i < array.length; i++)
             {
-                result += ", " + array[i];
+                if(array[i] != 0)
+                {
+                    result += ", " + array[i];
+                }
             }
         }
         return result + "}";
@@ -107,23 +117,18 @@ public class Sequence
     
     public static void main(String[] args)
     {
-    	in = new Scanner(System.in);
-        Sequence array = new Sequence(new int[]{1, 2, 3, 4, 5});
-        
-        int start = in.nextInt();
-        array.slice(start);
-        
+        Sequence a = new Sequence(new int[]{1, 2, 3, 4, 5});      
         // some informal testing
-//        System.out.println("a.slice(0)->" + a.slice(0));
-//        System.out.println("a.slice(1)->" + a.slice(1));
-//        System.out.println("a.slice(-1)->" + a.slice(-1));
-//        System.out.println("a.slice(-2)->" + a.slice(-2));
-//        System.out.println("a.slice(5)->" + a.slice(5));
-//        System.out.println("a.slice(-5)->" + a.slice(-5));
-//        System.out.println("a.slice(0, 5, 2)->" + a.slice(0, 5, 2));
-//        System.out.println("a.slice(1, 5, 2)->" + a.slice(1, 5, 2));
-//        System.out.println("a.slice(1, 5, 3)->" + a.slice(1, 5, 3));
-//        System.out.println("a.slice(2, 5, 3)->" + a.slice(2, 5, 3));
-//        System.out.println("a.slice(-1, -6, -1)->" + a.slice(-1, -6, -1));
+        //System.out.println("a.slice(0)->" + a.slice(0));
+        //System.out.println("a.slice(1)->" + a.slice(1));
+        //System.out.println("a.slice(-1)->" + a.slice(-1));
+        //System.out.println("a.slice(-2)->" + a.slice(-2));
+        //System.out.println("a.slice(5)->" + a.slice(5));
+        //System.out.println("a.slice(-5)->" + a.slice(-5));
+        //System.out.println("a.slice(0, 5, 2)->" + a.slice(0, 5, 2));
+        //System.out.println("a.slice(1, 5, 2)->" + a.slice(1, 5, 2));
+        //System.out.println("a.slice(1, 5, 3)->" + a.slice(1, 5, 3));
+        //System.out.println("a.slice(2, 5, 3)->" + a.slice(2, 5, 3));
+        //System.out.println("a.slice(-1, -6, -1)->" + a.slice(-1, -6, -1));
     }
 }
